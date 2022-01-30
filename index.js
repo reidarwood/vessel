5cf671796dfb7821712a7af3ed355feb90c08cb7
Andrew Boden <andrew.boden@hotmail.com>
2022-01-29 21:19:14 -0500

    correct pos

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let map;
 function initMap() {
   const mapOptions = {
     zoom: 8,
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 41.8239, lng: -71.4128 },
   };
 
   map = new google.maps.Map(document.getElementById("map"), mapOptions);
@@ -15,10 +15,22 @@ function initMap() {
 
   const marker = new google.maps.Marker({
     // The below line is equivalent to writing:
-    // position: new google.maps.LatLng(-34.397, 150.644)
-    position: { lat: -34.397, lng: 150.644 },
+    // position: new google.maps.LatLng()
+    position: { lat: 41.8239, lng: -71.4128 },
     map: map,
   });
+
+  function showPosition(position) {
+    var markerPosition={};
+    markerPosition.lat=position.coords.latitude;
+    markerPosition.lng=position.coords.longitude;
+
+    // Create a marker and place it on the map
+    var marker = new google.maps.Marker({
+      position: markerPosition,
+      map: map
+    });
+  }
   // You can use a LatLng literal in place of a google.maps.LatLng object when
   // creating the Marker object. Once the Marker object is instantiated, its
   // position will be available as a google.maps.LatLng object. In this case,
